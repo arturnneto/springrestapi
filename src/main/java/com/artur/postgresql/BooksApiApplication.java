90cf59d3f16cb9f3fc38c6701a5fedaf2002b936
Artur <arturjorgean@gmail.com>
2024-06-07 19:37:29 -0300

    Refactor: Application name changed.

--- src/main/java/com/artur/postgresql/BooksApiApplication.java
+++ src/main/java/com/artur/postgresql/BooksApiApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Log
-public class PostgresqlApplication  {
+public class BooksApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PostgresqlApplication.class, args);
+		SpringApplication.run(BooksApiApplication.class, args);
 	}
 
 }
